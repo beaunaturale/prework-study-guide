39bdb9dfae375033ac84341a9e488e7624532bc5
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-02-12 23:44:07 -0600

    fix typo

--- assets/script.js
+++ assets/script.js
@@ -24,4 +24,4 @@ if (randomTopic === 'HTML') {
 console.log('Here are the topics we learned through Prework:');
 listTopics()
 console.log('Which topic should we study first?');
-selectTopic()
+selectTopic();
